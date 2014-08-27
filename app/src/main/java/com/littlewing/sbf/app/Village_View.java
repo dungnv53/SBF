20ffdb8894e745853d207297d05456fa60411e56
dungnv53 <vandung_17@yahoo.com>
2014-08-28 00:41:10 +0700

    Remove enemy in village view

--- app/src/main/java/com/littlewing/sbf/app/Village_View.java
+++ app/src/main/java/com/littlewing/sbf/app/Village_View.java
@@ -40,18 +40,6 @@ class Village_View extends SurfaceView implements SurfaceHolder.Callback {
      */
     class VillageThread extends Thread {
         private Bitmap[] mHeroMoving = new Bitmap[7];
-        private Bitmap[] mEnemy = new Bitmap[5];
-
-        private Bitmap[] mBoss = new Bitmap[5];
-        private Bitmap[] item = new Bitmap[5];
-
-        private Bitmap snow_h;
-        private Bitmap snow_shadow;
-
-        private Bitmap[] img_Special = new Bitmap[4];
-        private Bitmap img_sp1;
-        private Bitmap img_sp2;
-        private Bitmap img_sp3;
         /**
          * dung luu 3 kieu spec tuy vao mana
          * Dung ra fai dung RMS nhung o day tam test cai int nay da.
@@ -126,7 +114,6 @@ class Village_View extends SurfaceView implements SurfaceHolder.Callback {
         private Random rnd = new Random();
 
         public static final int GAME_THREAD_DELAY = 600;
-        private Donald donald;
         /**
          * Mang donald luie
          */
@@ -137,10 +124,6 @@ class Village_View extends SurfaceView implements SurfaceHolder.Callback {
 
         private Bitmap allclear;
 
-        private int[] test_snow_e_y = new int[] {70, 70, 70};
-
-        private int win_sound_flag = 0;
-        private int lose_flag_sound = 0;
 
         private Bitmap v;
 
@@ -160,38 +143,6 @@ class Village_View extends SurfaceView implements SurfaceHolder.Callback {
             mHeroMoving[5] = BitmapFactory.decodeResource(res, R.drawable.hero_vic3);
             mHeroMoving[6] = BitmapFactory.decodeResource(res, R.drawable.hero_lose3);
 
-            mEnemy[0] = BitmapFactory.decodeResource(res, R.drawable.enemy00_3);
-            mEnemy[1] = BitmapFactory.decodeResource(res, R.drawable.enemy01_3);
-            mEnemy[2] = BitmapFactory.decodeResource(res, R.drawable.enemy02_3);
-            mEnemy[3] = BitmapFactory.decodeResource(res, R.drawable.enemy03_3);
-            mEnemy[4] = BitmapFactory.decodeResource(res, R.drawable.enemy00_3);
-
-            mBoss[0] = BitmapFactory.decodeResource(res, R.drawable.boss20_3);
-            mBoss[1] = BitmapFactory.decodeResource(res, R.drawable.boss21_3);
-            mBoss[2] = BitmapFactory.decodeResource(res, R.drawable.boss22_3);
-            mBoss[3] = BitmapFactory.decodeResource(res, R.drawable.boss23_3);
-
-            mBoss[4] = BitmapFactory.decodeResource(res, R.drawable.boss20_3);
-
-            snow_h = BitmapFactory.decodeResource(res, R.drawable.item3_0);
-            snow_shadow = BitmapFactory.decodeResource(res, R.drawable.shadow0_3);
-
-            // Mana hero cung voi ban tay = 2 PNG
-            img_Special[0] = BitmapFactory.decodeResource(res, R.drawable.special0_3);
-            img_Special[1] = BitmapFactory.decodeResource(res, R.drawable.special1_3);
-            img_Special[2] = BitmapFactory.decodeResource(res, R.drawable.special2_3);
-
-            // sp la 3 special tuy vao MANA, co the cho vo mang;
-            img_sp1 = BitmapFactory.decodeResource(res, R.drawable.sp1_3);
-            img_sp2 = BitmapFactory.decodeResource(res, R.drawable.sp2_3);
-            img_sp3 = BitmapFactory.decodeResource(res, R.drawable.sp3_3);
-
-            item[0] = BitmapFactory.decodeResource(res, R.drawable.item3_0);
-            item[1] = BitmapFactory.decodeResource(res, R.drawable.item1_3);
-            item[2] = BitmapFactory.decodeResource(res, R.drawable.item2_3);
-            item[3] = BitmapFactory.decodeResource(res, R.drawable.item3_3);
-            item[4] = BitmapFactory.decodeResource(res, R.drawable.item4_3);
-
             // cache handles to our key sprites & other drawables
             mTitleImage = BitmapFactory.decodeResource(res, R.drawable.title_bg_hori);
             mTitleImage = Bitmap.createScaledBitmap(mTitleImage, scr_width, (int)(scr_height/2), true);
@@ -199,6 +150,7 @@ class Village_View extends SurfaceView implements SurfaceHolder.Callback {
             // load background image as a Bitmap instead of a Drawable b/c
             // we don't need to transform it and it's faster to draw this way
             mBackgroundImage = BitmapFactory.decodeResource(res, R.drawable.village2);
+            mBackgroundImage = Bitmap.createScaledBitmap(mBackgroundImage, scr_width, (int)(scr_height/2), true);
 
             allclear = BitmapFactory.decodeResource(res, R.drawable.allclear);
             allclear = Bitmap.createScaledBitmap(allclear, scr_width, (int)(scr_height/2), true);
@@ -208,13 +160,6 @@ class Village_View extends SurfaceView implements SurfaceHolder.Callback {
             // Use the regular lander image as the model size for all sprites
             mLanderWidth = mTitleImage.getWidth();
             mLanderHeight = mTitleImage.getHeight();
-
-            donald = new Donald(50, 50, mBoss);
-            for(int kk = 0; kk < 3; kk ++) {
-                luie[kk] = new Donald (60*kk, kk*25, mEnemy);
-            }
-            bomb = new Bomb (50, 30, context);
-            bomb.setImage(item);
         }
 
         /**
@@ -418,45 +363,6 @@ class Village_View extends SurfaceView implements SurfaceHolder.Callback {
             return handled;
         }
 
-        /*
-         * Check hero fire hit enemy, enemy lose hp. fire_step is snow in grid to check hit target.
-         * hp_lose is value of hp lose if hero fire hit.
-         * mTop: position reset for enemy.
-         * TODO: move to class Donlad instead of lie here.
-         */
-        public void hitTarget(Donald dn, int fire_step, int hp_lose) {
-            int mTop = (scr_height-y_bound);      // Vị trí phía trên màn hình game đối với player (hero).
-            int mLeft = h_x + 20; // vi snow lech ra 1 chut
-            if ((dn.getDonaldX()+20 >= mLeft) && (dn.getDonaldX() - 10) <= mLeft) {
-                if( (dn.getDonaldY()+10 >= (180 - 12*fire_step)) && (dn.getDonaldY()-20) <= (180 - 12*fire_step)) {
-                    dn.setHp(dn.getHp() - hp_lose);
-                    mTop = 180; // TODO y ?
-                    m_snow_fire = 0;
-                }
-            }
-        }
-
-        /* Draw rectangle | show hp (percent) like in other game: AOE (horizontal).
-         * TODO nhieu so hardcode
-        * */
-        public void drawEnemy(Canvas cv, Donald huey, int snow_e_y_idx, int rand_donald_y, int h_y_step) {
-            test_snow_e_y [snow_e_y_idx] += 6;
-            if(test_snow_e_y[snow_e_y_idx] >= 205)
-                test_snow_e_y[snow_e_y_idx] = 70;
-            cv.drawBitmap(snow_h, huey.getDonaldX(), test_snow_e_y[snow_e_y_idx]-22 + rand_donald_y, null); // 22 la distance giua snow va shadow
-            cv.drawBitmap(snow_shadow, huey.getDonaldX(), test_snow_e_y[snow_e_y_idx], null);
-
-            if ((h_x - 8) <= huey.getDonaldX() && ((h_x + 8) >= huey.getDonaldX())
-                    && (h_y - h_y_step <= test_snow_e_y[snow_e_y_idx]) && (h_y + h_y_step >= test_snow_e_y[snow_e_y_idx])) {
-                h_hp -= 2;
-            }
-            if ( (h_x <= (huey.getDonaldX() + 8)) && (h_x >= (huey.getDonaldX() - 22))) {
-                if ( (snow_h_y <= (huey.getDonaldY() + 30)) && (snow_h_y >= (huey.getDonaldY() - 10))) {
-                    huey.setHp(huey.getHp() - 2);
-                    snow_h_y = 200; // fai set lai snow_y ko thi hp mat lien tuc
-                }
-            }
-        }
         /**
          * Draws the hero, enemy, and background to the provided
          * Mau cua enemy van chua chuan: Hp giam lien tuc nhu kieu loop neu trung dan nhieu
@@ -466,112 +372,17 @@ class Village_View extends SurfaceView implements SurfaceHolder.Callback {
         public void doDraw(Canvas canvas) {
             if (mMode == STATE_RUNNING) {
                 canvas.drawBitmap(mBackgroundImage, 0, 0, null);
-                boss_attack(canvas);
-//            draw_enemy(canvas);
-                donald.act(1, scr_width, scr_height);
-                donald.move();
                 Paint paint = newPaint(Color.RED, Style.FILL_AND_STROKE, 0);
                 // In Running mode
 
                 canvas.drawBitmap(mHeroMoving[mHeroIndex], h_x, h_y, null);
 
-                if (donald.getHp() > 0) {
-                    canvas.drawBitmap(donald.getBossImage(), donald.getDonaldX(), 85, null);
-                    canvas.drawRect((float) donald.getDonaldX()-5, 126 - (donald.getHp()*9/11), (float) donald.getDonaldX(), 126, paint);
-                }
-                for(int ii = 0; ii < 3; ii ++) {
-                    luie[ii].act(1, scr_width, scr_height);
-                    luie[ii].move();
-
-                    luie[ii].setBomb(bomb);
-                    luie[ii].item.dropBomb(luie[ii].getDonaldY(), 3/4*getHeight());
-
-                    if (luie[ii].getHp() > 0) {
-                        canvas.drawBitmap(luie[ii].getBossImage(), luie[ii].getDonaldX(), luie[ii].getDonaldY(), null);
-                        canvas.drawRect((float) luie[ii].getDonaldX()-5, luie[ii].getDonaldY() - (luie[ii].getHp()*4/7) + 30, (float) luie[ii].getDonaldX(), 30 + luie[ii].getDonaldY(), paint);
-                    }
-                }
-                // Draw the speed gauge, with a two-tone effect
-                canvas.save();
-
-                int snow_h_x = h_x + 12;   // snow_h_x va y fai ko dc change du xLeft top change
-                if (m_snow_fire == 10) {
-//            	make_attack(canvas);
-                    int mTop = (scr_height-y_bound);      // Vị trí phía trên màn hình game đối với player (hero).
-                    snow_h_y -= 12;
-                    if (snow_h_y < 25) snow_h_y = (scr_height-y_bound);
-//        		canvas.drawBitmap(snow_h, snow_h_x, snow_h_y-22, null);
-                    int rand_img = get_random(4);
-                    canvas.drawBitmap(bomb.getImage(2), h_x + 22, snow_h_y-22, null); // 22 la distance giua snow va shadow
-                    canvas.drawBitmap(snow_shadow, h_x + 22, snow_h_y, null);
-
-                    for (int ii = 0; ii <= (mTop/12); ii ++) {
-                        hitTarget(donald, ii, 1);
-                        hitTarget(luie[0], ii, 1);
-                        hitTarget(luie[1], ii, 1);
-                        hitTarget(luie[2], ii, 1);
-
-                        m_snow_fire = 0;
-                    } // end for loop
-                }
-                if (donald.getHp() <= 0) {  // WINNING
-                    if (luie[0].getHp() <= 0 && (luie[1].getHp() <= 0) && (luie[2].getHp() <= 0)) {
-                        mMode = STATE_WIN;
-                    }
-                }
-                // Boss attack
-                if (donald.getHp() >= 0) {
-                    test_snow_h_y += 6;
-                    if (test_snow_h_y >= (scr_height-y_bound))    // biên cho item bay tới.
-                        test_snow_h_y = 80;
-
-                    canvas.drawBitmap(snow_h, donald.getDonaldX() + get_random1(10), test_snow_h_y-22, null); // 22 la distance giua snow va shadow
-                    canvas.drawBitmap(snow_shadow, donald.getDonaldX(), test_snow_h_y, null);
-                    if ((h_x + 12) >= donald.getDonaldX() && ((h_x - 12) <= donald.getDonaldX())
-                            && (h_y + 12 >= test_snow_h_y) && (h_y - 12 <= test_snow_h_y)) {
-                        h_hp -= 4;
-                    }
-                }
-                // check snow hit hero
-                // Van chua su dung dc tinh nang isDestroyed cua Bomb Class de tranh viec hp giam lien tuc do snow ko huy.
-
-                // hero die ?
-                if (h_hp <= 0) {
-                    mMode = STATE_LOSE;
-                }
-                // enemy attack
-                // cause can not call e_attack_ai() propertly
-                if(luie[0].getHp() > 0) {
-                    drawEnemy(canvas, luie[0], 0, get_random1(8), 18);
-                }
-                if(luie[1].getHp() > 0) {
-                    drawEnemy(canvas, luie[1], 1, get_random1(8), 10);
-                }
-                if(luie[2].getHp() > 0) {
-                    drawEnemy(canvas, luie[2], 2, 0, 8);
-                }
-//            	draw special
-                if (use_special == 1) {
-                    use_special(canvas);
-//            			for(int ii = 0; ii <=3 ; ii ++) {
-//            				luie[ii].setHp(luie[ii].getHp() - 8);
-//            			}
-//            			donald.setHp(donald.getHp() - 8);
-
-                }
-//            	canvas.drawBitmap(mBackgroundImage, 0, 0, null);
+                int mTop = (scr_height-y_bound);      // Vị trí phía trên màn hình game đối với player (hero).
                 canvas.save();
 
             } // end running draw
 
             else if(mMode == STATE_LOSE) {
-                lose_flag_sound ++;
-                mpx = MediaPlayer.create(mContext, R.raw.s_lose);
-                if (lose_flag_sound  == 1) {
-                    mpx.start();
-                } else {
-                    if(mpx != null) mpx.stop();
-                }
                 canvas.drawBitmap(mHeroMoving[6], h_x, h_y, null);
                 String text = "You lose ...";
                 Paint p = new Paint();
@@ -579,8 +390,6 @@ class Village_View extends SurfaceView implements SurfaceHolder.Callback {
                 canvas.drawText(text, h_x - 5, h_y - 40, p);
 //            	canvas.restore();
             } else  if(mMode == STATE_WIN) {
-                win_sound_flag++; if(win_sound_flag ==1) { mpx.start(); } else { stopSound(); };
-
                 String text = "Victory !... \n";
                 Paint p = newPaint(Color.RED, null, 35);
                 canvas.drawBitmap(allclear, 0, 30, null);
@@ -598,98 +407,6 @@ class Village_View extends SurfaceView implements SurfaceHolder.Callback {
             }
             canvas.save();
             canvas.restore();
-        }      // end doDraw()
-
-        /** ve special bay ngang man hinh roi quay lai 1 lan roi bien mat; */
-        public void use_special(Canvas canvas) {
-//        	playHitTargetSound();
-            int sp_Left = 200;
-            int sp_Top = 100;
-            for (int i = 0; i < 200; i ++) {
-                sp_Left --;
-                canvas.drawBitmap(img_sp3, i, sp_Top, null); // if (sp_Left >= 200) use_special = 0;
-            }
-            if (sp_Left <= 1) {
-                for (int j = 200; j > 0; j --)
-                {
-                    canvas.drawBitmap(img_sp2, j, sp_Top, null);
-                }
-                use_special = 0;
-            }
-        }
-
-        /** draw snow ball from hero x, y to the target
-         1st we make target length is random n then use the POWER later
-         */
-        public void make_attack(Canvas canvas) {
-//        	playFiringSound();
-            int yTop = mCanvasHeight - ((int) h_y + mLanderHeight);
-            int xLeft = ((int) h_x - mLanderWidth / 2) + 20;
-            for (int ii = 0; ii <= (yTop/20); ii ++)
-            {
-                canvas.drawBitmap(snow_h, xLeft, 180 - (20*ii), null);
-                canvas.drawBitmap(snow_shadow, xLeft, 180 -(20*ii) + 22, null);
-            }
-            // if (yTop < 25) ...
-            m_snow_fire = 0;
-        }
-
-        public void e_attack(Canvas canvas, int targetX, int targetY) {
-            // dat co flag de chi cho enemy nem tuyet khi snow da ve vi tri dich
-            // co se dc bat lai khi snow trung dich hay roi xuong
-            for (int ii = 0; ii <= (targetY/20); ii ++)
-            {
-                canvas.drawBitmap(snow_h, targetX,  (20*ii), null);
-                canvas.drawBitmap(snow_shadow, targetX, (20*ii) + 22, null);
-            }
-            // Test draw snow gap
-            canvas.drawBitmap(snow_h, targetX += 3, targetX += 3, null);
-        }
-        public void e_attack_ai(Canvas canvas, int enemy_x, int enemy_y, int hero_x, int hero_y) {
-            int delta_x = (int)(Math.abs(enemy_x - hero_x));
-            int delta_y = (int)(Math.abs(enemy_y - hero_y)); // tinh duong cheo tao boi hero va enemy
-            int c = (int) (Math.sqrt(delta_x*delta_x + delta_y*delta_y));
-            // Tinh Delta X, Delta Y de ve duong dan cho enemy nem vao hero
-            int d_x = 12 * delta_x / c; // d_x de cho snow bay lech theo phuong ngang
-            int d_y = 12 * delta_y / c;
-            int snow_e_xx = enemy_x;  // de ve trog canvas
-            int snow_e_yy = enemy_y;
-            if(e_snow_fired == 10) {
-                snow_e_xx += d_x;
-                snow_e_yy += d_y;
-
-                canvas.drawBitmap(snow_h, snow_e_xx, snow_e_yy-22, null); // 22 la distance giua snow va shadow
-                canvas.drawBitmap(snow_shadow, snow_e_xx, snow_e_yy, null);
-            }
-            if ( snow_e_yy > 205 ){
-                e_snow_fired = 0;
-            }
-            e_snow_fired = 10;
-        }
-        /**
-         * Them ham boss_attack() voi random target, sound play
-         * test hp ... va vi tri nem
-         * ko run co le do chi dc goi 1 lan
-         * Neu dat vao ham run() chay chung voi doDraw() thi lam game speed nhanh gap co 2x
-         * -> tam thoi cho chay trong ham doDraw()
-         */
-        public void boss_attack(Canvas canvas) {
-            int width = getWidth();     // game screen width
-            int height = getHeight();   // game screen height
-            int h_bound = height/6;     // bound head n bottom of the screen
-            int step = height/15;       // step of snowball over screen
-
-            if (donald.getHp() >= 0) {
-                test_snow_h_y += step;
-                if (test_snow_h_y >= (height - h_bound))     // enemy snow pass hero position
-                    test_snow_h_y = h_bound/2;
-//				playFiringSound();
-//				playHitTargetSound();
-                canvas.drawBitmap(mBackgroundImage, 0, 0, null);
-                canvas.drawBitmap(snow_h, donald.getDonaldX() + step, test_snow_h_y-(step/2), null); // 22 la distance giua snow va shadow
-                canvas.drawBitmap(snow_shadow, donald.getDonaldX() + step, test_snow_h_y, null);
-//				Village_View.this.invalidate();
-            }
         }
         /**
          * Figures the lander state (x, y, fuel, ...) based on the passage of
@@ -867,27 +584,6 @@ class Village_View extends SurfaceView implements SurfaceHolder.Callback {
                 retry = false;
             } catch (InterruptedException e) {
             }
-        }
-    }
-
-    public  void playTitleSound(){
-        mpx = MediaPlayer.create(mContext, R.raw.sbf_win);
-        mpx.start();
-        mpx.setLooping(false);
-    }
-    public  void playFiringSound(){
-        mpx = MediaPlayer.create(mContext, R.raw.s_fire);
-        mpx.start();
-    }
-    public  void playHitTargetSound(){
-        mpx = MediaPlayer.create(mContext, R.raw.s_hit);
-        mpx.start();
-    }
-    public  void stopSound(){
-        if(mpx != null){
-            mpx.stop();
-            mpx.release();
-            mpx = null;
         }
     }
 
